33a455cd1d3484fe2a3cb2f466a1066d9e78f130
dima2015rl <dimachudakov2003@mail.ru>
2025-03-10 18:28:21 +0300

    fix

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-DATABASE_URL = "postgresql+asyncpg://postgres:admin@localhost:5432/postgres"
+DATABASE_URL = "postgresql+asyncpg://postgres:tYPGoinBoiaLFEXOKRgcnDlQKiIpFnTJ@postgres.railway.internal:5432/railway"
 secret_key = "asdkljhadsflkhglkajflklkdfsglkdghdghdklfsjghklhlkcvblkxcvhbjclkvxjbhlckxjvbhcvlkbj"
 algoritm = "HS256"
+
+
