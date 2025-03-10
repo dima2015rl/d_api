5b11eb6296f83db7dae49f68006ab880e320df72
dima2015rl <dimachudakov2003@mail.ru>
2025-03-10 19:36:32 +0300

    fix3

--- src/shop/dao/cart_dao.py
+++ src/shop/dao/cart_dao.py
@@ -13,20 +13,10 @@ class CartDAO(BaseDAO):
         async with async_session() as session:
             query = select(Cart).filter_by(user_id=user_id).options(joinedload(Cart.cart_products))
             result = await session.execute(query)
-            cart = result.scalar_one_or_none()
+            cart = result.unique().scalar_one_or_none()
 
             if not cart:
                 cart = await CartDAO.add(Cart(user_id=user_id))
-
-            return {
-                "id": cart.id,
-                "user_id": cart.user_id,
-                "cart_products": [
-                    {
-                        "id": cp.id,
-                        "product_id": cp.product_id,
-                        "quantity": cp.quantity,
-                    }
-                    for cp in cart.cart_products  # cart_products будет пустым, но это ок
-                ],
-            }
+                result= await session.execute(select(Cart).filter_by(user_id=user_id))
+                cart = result.scalar_one_or_none()
+            return cart
